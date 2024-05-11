0c9f7c783d897ebc0a335f29c19be316c790a1fd
Juan Carlos Largo <134282467+imlargo@users.noreply.github.com>
2024-05-11 17:11:43 +0000

    fix boletin styles

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,9 @@
 import { defineConfig } from 'astro/config';
+import sectionize from '@hbsnow/rehype-sectionize';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+    markdown:{
+        rehypePlugins: [[sectionize, { level: 'h2' }]],
+    },
+});
