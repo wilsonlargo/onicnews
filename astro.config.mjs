d1be93b5bed361a7afb9d8956d9c6c9e3043a7f5
Juan Carlos Largo <134282467+imlargo@users.noreply.github.com>
2024-05-15 03:22:05 +0000

    add footnotes

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,5 +5,19 @@ import sectionize from '@hbsnow/rehype-sectionize';
 export default defineConfig({
     markdown:{
         rehypePlugins: [[sectionize, { level: 'h2' }]],
+        remarkRehype: {
+            allowDangerousHtml: true,
+
+            clobberPrefix: "referencia-",
+            footnoteBackContent: ' ',
+
+            footnoteLabel: "Pie de pagina",
+            footnoteLabelTagName: "span",
+            footnoteLabelProperties: { class: "d-none" },
+
+            footnoteBackLabel(referenceIndex, rereferenceIndex) {
+                  return `Regresar a referencia #${referenceIndex + 1}${rereferenceIndex > 1 ? `-${rereferenceIndex}` : ''}`
+            },
+        },
     },
 });
